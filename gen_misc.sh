069aef1a9ef750449b3529df4c082c0367288eef
Pontus Oldberg <pontus.oldberg@invector.se>
2015-06-10 21:01:50 +0200

    First commit. Cloned from Espressif repo and made to compile under SDK 1.1

---
+++ gen_misc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+make COMPILE=gcc
+if [ $? == 0 ];then
+  rm ../bin/eagle.app.v6.flash.bin ../bin/eagle.app.v6.irom0text.bin ../bin/eagle.app.v6.dump ../bin/eagle.app.v6.S
+
+  cd .output/eagle/debug/image
+
+  xtensa-lx106-elf-objdump -x -s eagle.app.v6.out > ../../../../../bin/eagle.app.v6.dump
+  xtensa-lx106-elf-objdump -S eagle.app.v6.out > ../../../../../bin/eagle.app.v6.S
+
+  xtensa-lx106-elf-objcopy --only-section .text -O binary eagle.app.v6.out eagle.app.v6.text.bin
+  xtensa-lx106-elf-objcopy --only-section .data -O binary eagle.app.v6.out eagle.app.v6.data.bin
+  xtensa-lx106-elf-objcopy --only-section .rodata -O binary eagle.app.v6.out eagle.app.v6.rodata.bin
+  xtensa-lx106-elf-objcopy --only-section .irom0.text -O binary eagle.app.v6.out eagle.app.v6.irom0text.bin
+
+  ../../../../../tools/gen_appbin.py eagle.app.v6.out 2 0 2 5
+
+  cp eagle.app.v6.irom0text.bin ../../../../../bin/
+  cp eagle.app.v6.flash.bin ../../../../../bin/
+else
+  echo "make error"
+fi
---
+++ gen_misc_plus.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -x
+# If xtensa toolchain is to be used make sure the environment variable
+# COMPILE is set to gcc before running this script.
+
+touch user/user_main.c
+make APP=$1
+if [ $? == 0 ];then
+  rm ../bin/upgrade/user$1.bin ../bin/upgrade/user$1.dump ../bin/upgrade/user$1.S
+
+  cd .output/eagle/debug/image/
+
+  xtensa-lx106-elf-objdump -x -s eagle.app.v6.out > ../../../../../bin/upgrade/user$1.dump
+  xtensa-lx106-elf-objdump -S eagle.app.v6.out > ../../../../../bin/upgrade/user$1.S
+
+  xtensa-lx106-elf-objcopy --only-section .text -O binary eagle.app.v6.out eagle.app.v6.text.bin
+  xtensa-lx106-elf-objcopy --only-section .data -O binary eagle.app.v6.out eagle.app.v6.data.bin
+  xtensa-lx106-elf-objcopy --only-section .rodata -O binary eagle.app.v6.out eagle.app.v6.rodata.bin
+  xtensa-lx106-elf-objcopy --only-section .irom0.text -O binary eagle.app.v6.out eagle.app.v6.irom0text.bin
+
+  ../../../../../tools/gen_appbin.py eagle.app.v6.out 2 0 2 5
+
+  ../../../../../tools/gen_flashbin.py eagle.app.flash.bin eagle.app.v6.irom0text.bin
+
+  cp eagle.app.flash.bin user$1.bin
+  cp user$1.bin ../../../../../bin/upgrade/
+
+  cd ../../../../../
+
+else
+  echo "make error"
+fi
